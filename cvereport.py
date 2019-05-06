793f5859a283b83ec55d972e4b07b2fa5a22e23c
Siva Kumar <sadhikar@redhat.com>
2019-05-06 12:53:06 +0530

    help text updated with examples

--- cvereport.py
+++ cvereport.py
@@ -15,8 +15,12 @@ def getArgs():
     if (len(arguments)>0):
         if (arguments[0]=="help" or arguments[0]=="h" or arguments[0]=="-help" or arguments[0]=="-h"):
             print ("Usage: Run the program without any arguments and data will be for today")
-            print ("You can pass FromDate to get data for a particular date")
-            print ("You can pass FromDate and ToDate to get data between two dates")
+            print ("Pass FromDate ONLY to get data for a particular date")
+            print ("Pass FromDate and ToDate to get data between two dates")
+            print ("------------------------------------------------------")
+            print ("Example 1: python3 cvereport.py (returns the open and closed CVEs today)")
+            print ("Example 2: python3 cvereport.py 2019-05-01 (returns open and closed CVEs on 1st May 2019)")
+            print ("Example 3: python3 cvereport.py 2019-05-01 2019-05-03 (returns open and closed CVEs from 1st May to 3rd May including both days)")
             sys.exit(1)
         CVE_DATE = parser.parse(arguments[0])
         if (len(arguments)==2):
